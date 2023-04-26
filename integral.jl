b3205d53f3c325a5ac982a53f5bb57fb797f0f60
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-04-25 21:47:00 -0700

    Update integral.jl

--- integral.jl
+++ integral.jl
@@ -3,11 +3,13 @@ function integral(func::Function, start, stop, increment = 0.00001)
 
     sum = 0
     for i in ran
+        # Make sure the rectangle is always below the curve
         sum += increment * min(func(i), func(i + increment))
     end
     return sum
 end
 
+# Some input function f(x)
 f = x -> ℯ^-x
 
 println(integral(f, 0, 1000))
